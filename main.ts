aaff1a8114966246d07ec8c07e37a77ce20fd563
Thomas Wölfle <woelfle@posteo.de>
2022-10-10 14:37:02 +0200

    Initial Version

--- main.ts
+++ main.ts
@@ -1,85 +1,159 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, MarkdownPostProcessorContext, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface MeetingTimerSettings {
+	autoLog: boolean;
+	dateFormat: string;
+	logDateLinking: string;
+	duration: string;
+	notify: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: MeetingTimerSettings = {
+	autoLog: false,
+	dateFormat: 'YYYY-MM-DD',
+	logDateLinking: 'none',
+	duration: '00:25',
+	notify: '5'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+type Time = {
+	h: number
+	m: number
+	s: number
+}
+
+function leadingZeros(num: number, totalLength: number): string {
+	return String(num).padStart(totalLength, '0');
+}
+
+function timeToString(t:Time) {
+	const hours = leadingZeros(t.h, 2)
+	const minutes = leadingZeros(t.m, 2)
+	const seconds = leadingZeros(t.s, 2)
+	return (`${hours}:${minutes}:${seconds}`)
+}
+
+export default class NoteTimer extends Plugin {
+	settings: MeetingTimerSettings;
+	timerInterval : undefined | number = undefined
+
+	nextOpenLine(positions:number[], target:number) {
+		// target: identifies the table location
+		// +2: next 2 line breaks are md table column titles, and format lines
+		return positions[positions.findIndex(n => n > target)+2]
+	}
+
+	
+	resolveNotify(src:string, setting:string) {
+		let result = setting;
+		if(src.toLowerCase().contains('notify')) {
+			const arr = src.match(/^notify:(.)*$/gm);
+			if(arr != null && arr.length > 0) {
+				result = arr[0];
+			} else {
+				console.log("No notify configured. Using default")
+			}
+		}
+		console.log(`Resolved notify: ${result}`)
+		return result
+	}
+
+	resolveDuration(src:string, setting:string) {
+		const key = `duration`
+		console.log(`resolve ${key}: ${src} ${setting}`)
+		let result = setting;
+		if(src.toLowerCase().contains(key)) {
+			const arr = src.match(/^duration:(.)*$/gm);
+			if(arr != null && arr.length > 0) {
+				const line = arr[0];
+				result = line.slice(line.indexOf(`:`)+1, line.length).trim()
+			} else {
+				console.log(`No ${key}  configured. Using default`)
+			}
+		}
+		console.log(`Resolved ${key}: ${result}`)
+		return result
+	}
+
+	async extractConfig(ctx:MarkdownPostProcessorContext) {
+		const actFile = this.app.vault.getFiles().find(file => file.path === ctx.sourcePath)
+		if(actFile != undefined) {
+			const curString = await this.app.vault.read(actFile);
+			const timerBlockStart = curString.indexOf("```meeting-timer")
+			const timerBlockEnd = curString.slice(timerBlockStart, curString.length).indexOf("```", 3) + 3 + timerBlockStart
+			const timerConfig = curString.substring(timerBlockStart, timerBlockEnd)
+			console.log(timerConfig)
+			const duration = this.resolveDuration(timerConfig, this.settings.duration)
+			const notify = this.resolveNotify(timerConfig, this.settings.notify)
+			console.log(duration)
+			console.log(notify)
+		}
+	}
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		this.registerMarkdownCodeBlockProcessor("meeting-timer", (src,el,ctx) => {
+			this.extractConfig(ctx)			
+			const time:Time = {h:0,m:25,s:0}
+			const stringTime = () => {
+				return (timeToString(time))
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			const timeDisplay = el.createEl("span", { text: stringTime()})			
+			const buttonDiv = el.createDiv({ cls: "meeting-timer-button-group"})
+			const start = buttonDiv.createEl("button", { text: "start", cls: "meeting-timer-start" })
+			const reset = buttonDiv.createEl("button" ,{ text: "reset", cls: "meeting-timer-reset"})
+
+			const runTimerBackward = () => {
+				time.s--
+				if (time.s === -1) {
+					time.s = 59
+					time.m--
+				}
+				if (time.m === -1) {
+					time.m = 59
+					time.h--
+				}
+				timeDisplay.setText(stringTime())
+			}
+
+			let isRunning = false
+
+			const timerControl = () => {
+				if(!isRunning){
+					isRunning = true
+					start.setText("pause")
+					window.clearInterval(this.timerInterval)
+					this.timerInterval = window.setInterval(runTimerBackward, 1000)
+				} else if (isRunning){
+					isRunning = false
+					start.setText("start")
+					clearInterval(this.timerInterval)
+				}
+				if(this.timerInterval != undefined) {
+					this.registerInterval(this.timerInterval)
 				}
 			}
-		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+			start.onclick = () => timerControl()
+			reset.onclick = () => {
+				time.h = 0
+				time.m = 25
+				time.s = 0
+				timeDisplay.setText(stringTime())
+			}
+		})
+		
+
+		await this.loadSettings();
+
+		this.addSettingTab(new NoteTimerSettingsTab(this.app, this));
+
 	}
 
 	onunload() {
-
+		console.log('unloading meeting plugin');
 	}
 
 	async loadSettings() {
@@ -91,26 +165,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class NoteTimerSettingsTab extends PluginSettingTab {
+	plugin: NoteTimer;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: NoteTimer) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,18 +178,17 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Obsidian Meeting Timer Settings'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Meeting duration')
+			.setDesc('The default duration of a meeting. E.g. 30 minutes. The format is HH:MM')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setValue(this.plugin.settings.duration)
+				.setPlaceholder('HH:MM')
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
+					this.plugin.settings.duration = value
+					await this.plugin.saveSettings()
 				}));
 	}
 }
